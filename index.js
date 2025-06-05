77b35ee2898a30389ccfbb91b48313ca16c396b0
AnamulHaqueN <rafaielanam@gmail.com>
2025-06-05 21:01:42 +0600

    add uuid for posts as well as new posts

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 
+const {v4: uuidv4} = require("uuid");
 let posts = [
-    {
+    {   
+        id: uuidv4(),
         username: "ApnaCollege",
         content: "A learning Platform"
     },
 
     {
+        id: uuidv4(),
         username: "Shradha khapra",
         content: "Is an Instructor"
     },
 
     {
+        id: uuidv4(),
         username: "Anamul",
         content: "Is an learner of this platform"
     }
@@ -37,13 +41,20 @@ app.get("/posts/new", (req, res) => {
 
 // post request for add new post 
 app.post("/posts", (req, res) => {
+    let id = uuidv4();
     let {username, content} = req.body;
-    posts.push({username, content});
+    posts.push({id, username, content});
     // redirect the url to the posts path
     res.redirect("\posts"); // it use get request
 });
 
-
+// show id
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", {post});
+    // res.send("Respose is working !");
+})
 
 app.listen(port, () => {
     console.log(`App is listening port ${port}`);
