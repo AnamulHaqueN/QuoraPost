5140ed2057defb66c2566d9fbc0ea79d3e9e8c51
AnamulHaqueN <rafaielanam@gmail.com>
2025-06-06 11:42:55 +0600

    add functionalities for edit and delete posts

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const {v4: uuidv4} = require("uuid");
+const methodOverride = require("method-override");
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-const {v4: uuidv4} = require("uuid");
 let posts = [
     {   
         id: uuidv4(),
@@ -45,7 +48,7 @@ app.post("/posts", (req, res) => {
     let {username, content} = req.body;
     posts.push({id, username, content});
     // redirect the url to the posts path
-    res.redirect("\posts"); // it use get request
+    res.redirect("/posts"); // it use get request
 });
 
 // show id
@@ -54,8 +57,29 @@ app.get("/posts/:id", (req, res) => {
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs", {post});
     // res.send("Respose is working !");
-})
+});
 
+// update posts content by id
+app.patch("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post});
+}); 
+
+// delete/destroy post
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+}) 
 app.listen(port, () => {
     console.log(`App is listening port ${port}`);
 });
